3108b14112b709a55538bfdb34c96730c768d839
Alvaro Pareja-Lecaros <app6141@gmail.com>
2026-01-09 13:04:33 -0500

    add some docs, select for filtering by status (unconnected to state)

--- src/components/campaigns/CampaignsTable.tsx
+++ src/components/campaigns/CampaignsTable.tsx
@@ -15,15 +15,16 @@ import {
   VisibilityState, // for column visibility
 } from "@tanstack/react-table"
 
-import { Button } from "@/components/ui/button" // for sorting
 import { Input } from '@/components/ui/input' // for filtering
 
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu" // for column visibility
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 import {
   Table,
@@ -33,8 +34,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { TablePagination } from '../table/TablePagination'
-import { TableViewOptions } from '../table/TableViewOptions'
+import { TablePagination } from '../table/TablePagination' // for pagination
+import { TableViewOptions } from '../table/TableViewOptions' // for column visibility
 // import { TableViewOptionsSimple } from '../table/TableViewOptionsSimple'
 
 interface CampaignsTableProps<TData, TValue> {
@@ -50,6 +51,9 @@ export function CampaignsTable<TData, TValue>({
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]) // for filtering
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({}) // for column visibility
   const [rowSelection, setRowSelection] = useState({}) // for row selection
+
+  // todo: connect this to the select component
+  const [campaignStatusFilter, setCampaignStatusFilter] = useState<'All'|'Archived'|'Active'>('All')
 
   const table = useReactTable({
     data,
@@ -72,7 +76,7 @@ export function CampaignsTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex items-center py-4">
+      <div className="flex items-center justify-between py-4">
         {/* for filtering */}
         <Input
           placeholder="Filter campaigns..."
@@ -82,8 +86,24 @@ export function CampaignsTable<TData, TValue>({
           }
           className="max-w-sm"
         />
-        {/* for column visibility */}
-        <TableViewOptions table={table} />
+        <div className="flex">
+          <div className="px-4">
+            <Select defaultValue="All">
+              <SelectTrigger size="sm">
+                <SelectValue/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="Active">Active Campaigns</SelectItem>
+                  <SelectItem value="Archived">Archived Campaigns</SelectItem>
+                  <SelectItem value="All">All Campaigns</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          {/* for column visibility */}
+          <TableViewOptions table={table} />
+        </div>
       </div>
       <div className="overflow-hidden rounded-md border">
         <Table>
